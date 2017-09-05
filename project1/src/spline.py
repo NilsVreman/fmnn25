165eb778de23fe726c56ac758520ddab99820eb8
RykteT <nils.vreman@gmail.com>
2017-09-05 19:13:13 +0200

    Made spline.py a little bit more optimized by moving some code from public functions to private functions. also created some hidden variables.

--- project1/src/spline.py
+++ project1/src/spline.py
@@ -3,31 +3,46 @@ import numpy as np
 
 class spline:
 
-    def __init__(self):
-        pass
-
-    """
-    Calculate a value on the spline given the control points d and a position u (0-1)
-    
-    d:  Control points {(x_i, y_i)
-    u:  The point in which we want to evaluate the spline (0-1)
-    p:  Degree of the spline
-    """
-    def value(self, d, u, p=3):
-
-        #Return endpoints if u = 0 or 1
-        if u == 0: return d[0]
-        elif u == 1: return d[-1]
+    def __init__(self, d, p=3):
+        self.__d = d
+        self.__p = p
 
         #Create knots vector (L = K-2) and pad it with p (degree) repetitions on each side
         xi = np.zeros(len(d)-2+2*p)
         xi[-p:] = np.ones(p)
         xi[p:-p] = np.array([ i for i in np.linspace(0, 1, len(d)-2)])
+        self.__xi = xi
+
+    def __find_interval(self, u):
+        """
+        u:  The point in which we want to evaluate the spline (0-1)
+        return: A tuple: The interval index I, relevant control points d_i
+        """
+        I = np.searchsorted(self.__xi, u) - 1
+        d_i = np.array([self.__d[i-1] for i in range(I-self.__p+1, I+1+1)])
+        return I, d_i
+    
+    def value(self, u):
+        """
+        Calculate a value on the spline given the control points d and a position u (0-1)
+    
+        d:  Control points {(x_i, y_i)
+        u:  The point in which we want to evaluate the spline (0-1)
+        p:  Degree of the spline
+        return: value of spline in u
+        """
+        #Collects the control points and data points from the attributes
+        d = self.__d
+        p = self.__p
+        xi = self.__xi
+
+        #Return endpoints if u = 0 or 1
+        if u == 0: return d[0]
+        elif u == 1: return d[-1]
 
         #Find the index of the knot interval where u is located.
         #Put surrounding p+1 control points that are influencing the final value in a vector
-        I = np.searchsorted(xi, u) - 1
-        d_i = np.array([d[i-1] for i in range(I-p+1, I+1+1)])
+        I, d_i = self.__find_interval(u)
 
         #Evaluation
         for deg_lvl in range(0, p):
@@ -37,15 +52,16 @@ class spline:
 
         return d_i[p]
 
+
 if __name__ == '__main__':
-    sp = spline()
-    d = np.array([[0,0], [5,0], [5,5], [0,10]]).astype(float)   #Control points
+    d = np.array([[0,0], [5,0], [8, 3], [5,5], [0,10]]).astype(float)   #Control points
+    sp = spline(d)
     steps = 100                                                 #Nbr of steps to evaluate
     results = np.zeros([steps+1, 2])                            #All results for each step
 
     #Calculate the spline and put the results in a matrix
     for i in range(0, steps+1):
-        results[i,:] = sp.value(d, i/steps)
+        results[i,:] = sp.value(i/steps)
         print(results[i])
 
     plt.plot(results[:,0], results[:,1])    #Plot the spline
