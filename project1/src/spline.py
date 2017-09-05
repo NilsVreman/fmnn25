59815e0c0e2576672401b8673b28085493ab6eef
RykteT <nils.vreman@gmail.com>
2017-09-05 14:55:00 +0200

    Tried rewriting the spline.py class. almost working

--- project1/src/spline.py
+++ project1/src/spline.py
@@ -7,41 +7,30 @@ import pylab as pl
 class spline:
 
     def __init__(self):
-        print("hej")
+        pass
 
-
-    def value(self, I, u_ctrl, u, x0):
+    def value(self, xi, d, u, p=3):
         """
-        I: index of knot interval that contains x [u_I, u_{I+1}]
-        u_ctrl: array of ctrl points
-        u: the point in which we want to evaluate the spline
-        x: array of knot points
-        return: value s(u)
+        xi: grid points on u. Assume xi sorted and padded
+        d:  control points {(x_i, y_i)}_{i=0}^L
+        u:  the point in which we want to evaluate the spline
+        p:  degree of the spline
         """
-        x = [x0[0], x0[0], x0[0], x0[0], x0[1], x0[2], x0[3], x0[4], x0[4], x0[4], x0[4]]
-        deg = 3
-        d = [u_ctrl[i + I - deg] for i in range(0, deg+1)]
+        I = np.searchsorted(xi, u) - 1
+        d_i = np.array([d[i] for i in range(I-2, I+1+1)])
 
-        for deg_lvl in range(1, deg+1):
-            for index in range(deg, deg_lvl-1, -1):
-                alpha = (u - x[index+I-deg]) / (x[index+1+I-deg_lvl] - x[index+I-deg]) 
-                print((x[index+1+I-deg_lvl] - x[index+I-deg]), index+1+I-deg_lvl, index+I-deg)
-                d[index] = alpha*d[index] + (1.0 - alpha)*d[index-1]
+        for deg_lvl in range(0, p):
+            for depth in range(p, deg_lvl, -1):
+                alpha = (xi[I + depth - deg_lvl] - u) / (xi[I + depth - deg_lvl] - xi[I + depth - p])
+                d_i[depth] = alpha*d_i[depth-1] + (1-alpha)*d_i[depth]
 
-        return d[deg]
+        return d_i[p]
+        
 
-if __name__ =='__main__':
+if __name__ == '__main__':
+    xi = np.array([0, 0, 0, 0, 0.25, 0.5, 0.75, 1.0, 1.0, 1.0, 1.0])
+    d = np.array([0.1, 0.3, 0.4, 0.5, 0.7, 0.8, 0.9])
+
     f = spline()
-    x = np.array([1, 2, 3, 4, 5])
-    y = np.array([21, 42, 3, 14,19])
-    x_ctrl = np.array([1, 3, 5, 7, 8, 2, 4, 6])
-    y_ctrl = np.array([4, 6, 14, 53, 20, 40, 23, 19])
-    I = 5
-    sx = np.zeros([100])
-    sy = np.zeros([100])
-    for i in range(0, 100):
-        sx[i] = f.value(I, x_ctrl, 3+i/100, x)
-        sy[i] = f.value(I, y_ctrl, 3+i/100, y)
-
-    plt.plot(sx, sy, 'b^', x_ctrl, y_ctrl, 'gs', x, y, '+')
-    plt.show()
+    for i in np.arange(0.01, 0.99, 0.01):
+        print(f.value(xi, d, i))
