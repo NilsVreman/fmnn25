9d776f7d29e7436f3c222b61c159965b84fc7bda
RykteT <nils.vreman@gmail.com>
2017-10-04 14:00:35 +0200

    Fixed create_boundary_vec

--- project3/src/Heat.py
+++ project3/src/Heat.py
@@ -3,6 +3,10 @@ import scipy.linalg as spl
 
 class Heat():
 
+    """
+    length_x, length_y: room dimensions in length units
+    dx: grid point distance
+    """
     def _create_sys_matrix(self, length_x, length_y, dx):
         # calculations
         nbr_sq_x = int(length_x / dx)
@@ -28,13 +32,13 @@ class Heat():
 
 
     """
+    length_x, length_y: room dimensions in length units
     v1: Vector containing boundary values along left wall
     v2: Vector containing boundary values along top wall
     v3: Vector containing boundary values along right wall
     v4: Vector containing boundary values along bottom wall
+    dx: Grid point distance
     """
-
-
     def _create_boundary_vec(self, length_x, length_y, v1, v2, v3, v4, dx):
         # calculations
         nbr_sq_x = int(length_x / dx)
@@ -45,42 +49,50 @@ class Heat():
         # Create boundary condition vector
         b = np.zeros((nbr_sq_x - 1) * (nbr_sq_y - 1))
 
-        for i in range(0, len(v1) - 1):
+        # Adds boundary conditions from LEFT boundary
+        for i in range(0, len(v1) - 2):
             j = nbr * i
             b[j] = b[j] - v1[i + 1] / dx ** 2
 
-        for i in range(0, len(v3) - 1):
+        # Adds boundary conditions from RIGHT boundary
+        for i in range(0, len(v3) - 2):
             j = (i + 1) * nbr - 1
-            b[j] = b[j] - v2[i + 1] / dx ** 2
+            b[j] = b[j] - v3[i + 1] / dx ** 2
 
+        # Adds boundary conditions from TOP boundary
         for i in range(1, len(v2) - 1):
-            b[i] = b[i] - v2[i] / dx ** 2
+            b[i-1] = b[i-1] - v2[i] / dx ** 2
 
+        # Adds boundary conditions from BOTTOM boundary
         for i in range(1, len(v4) - 1):
-            b[len(b) - nbr - 1 + i] = b[len(b) - nbr - 1 + i] - v4[i] / dx ** 2
+            j = len(b) - nbr - 1 + i
+            b[j] = b[j] - v4[i] / dx ** 2
 
         return b
 
+    """
+    Calculates the room temperature according to the laplace equation
+    """
     def calculate_heat(self, length_x, length_y, v1, v2, v3, v4, dx):
         A = self._create_sys_matrix(length_x, length_y, dx)
         b = self._create_boundary_vec(length_x, length_y, v1, v2, v3, v4, dx)
 
-        print(b)
-
+        u = spl.solve(A, b)
+        return u
 
 
 if __name__ == '__main__':
     # input
     length_x = 1
-    length_y = 2
-    dx = 1 / 3
+    length_y = 1
+    dx = 1 / 4
 
     nbr_sq_x = int(length_x / dx)
     nbr_sq_y = int(length_y / dx)
-    v1 = -10 * np.ones(nbr_sq_y+1)
-    v2 = -15 * np.ones(nbr_sq_x+1)
-    v3 = -20 * np.ones(nbr_sq_y+1)
-    v4 = -25 * np.ones(nbr_sq_x+1)
+    v1 = 10 * np.ones(nbr_sq_y+1)
+    v2 = 15 * np.ones(nbr_sq_x+1)
+    v3 = 20 * np.ones(nbr_sq_y+1)
+    v4 = 25 * np.ones(nbr_sq_x+1)
 
     h = Heat()
     h.calculate_heat(length_x, length_y, v1, v2, v3, v4, dx)
